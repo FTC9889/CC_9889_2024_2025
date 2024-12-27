bc7509bcee62c35ebc2926877c797a837a4989a4
Daniel-9889 <147260536+Daniel-9889@users.noreply.github.com>
2024-12-26 23:09:56 -0500

    Created Some Auto Pathing

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -1,34 +1,58 @@
 package com.example.meepmeeptesting;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
 
 import org.rowlandhall.meepmeep.MeepMeep;
 import org.rowlandhall.meepmeep.roadrunner.DefaultBotBuilder;
 import org.rowlandhall.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
 public class MeepMeepTesting {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(800);
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
-                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(0, 0, 0))
-                        .forward(30)
-                        .turn(Math.toRadians(90))
-                        .forward(30)
-                        .turn(Math.toRadians(90))
-                        .forward(30)
-                        .turn(Math.toRadians(90))
-                        .forward(30)
-                        .turn(Math.toRadians(90))
+                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(-8, -60, Math.toRadians(-90)))
+                        .strafeTo(new Vector2d(-8, -30))
+                        .splineToLinearHeading(new Pose2d(-48, -40, Math.toRadians(90)), 90)
+                        .splineToLinearHeading(new Pose2d(-58, -54, Math.toRadians(45)), 45)
+                        .splineToLinearHeading(new Pose2d(-58, -40, Math.toRadians(90)), 90)
+                        .splineToLinearHeading(new Pose2d(-58, -54, Math.toRadians(45)), 45)
+                        .splineToLinearHeading(new Pose2d(-62, -4, Math.toRadians(90)), 90)
+                        .strafeTo(new Vector2d(-62, -55))
+                        .turn(Math.toRadians(-90))
+                        .strafeTo(new Vector2d(48, -62))
                         .build());
+
+        RoadRunnerBotEntity myBot2 = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(8, -60, Math.toRadians(-90)))
+                        .strafeTo(new Vector2d(8, -30))
+                        .splineToLinearHeading(new Pose2d(47, -48, Math.toRadians(90)), Math.toRadians(90))
+                        .strafeTo(new Vector2d(58, -48))
+                        .turn(Math.toRadians(-25))
+                        .lineToLinearHeading(new Pose2d(35, -60, Math.toRadians(90)))
+                        .splineToLinearHeading(new Pose2d(2, -30, Math.toRadians(-90)), Math.toRadians(90))
+                        .splineToLinearHeading(new Pose2d(35, -60, Math.toRadians(90)), Math.toRadians(-90))
+                        .splineToLinearHeading(new Pose2d(-2, -30, Math.toRadians(-90)), Math.toRadians(90))
+                        .splineToLinearHeading(new Pose2d(35, -60, Math.toRadians(90)), Math.toRadians(-90))
+                        .splineToLinearHeading(new Pose2d(-6, -30, Math.toRadians(-90)), Math.toRadians(90))
+                        .splineToLinearHeading(new Pose2d(35, -60, Math.toRadians(90)), Math.toRadians(-90))
+                        .splineToLinearHeading(new Pose2d(-10, -30, Math.toRadians(-90)), Math.toRadians(90))
+                        .splineToLinearHeading(new Pose2d(16, -60, Math.toRadians(0)), Math.toRadians(-90))
+
+                        .build());
+
 
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_INTOTHEDEEP_JUICE_DARK)
                 .setDarkMode(true)
                 .setBackgroundAlpha(0.95f)
                 .addEntity(myBot)
+                .addEntity(myBot2)
                 .start();
     }
 }
